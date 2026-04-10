668b4c3b04725570f5caf9aeac22e8427b6af4a9
Parth Joshi <parthnguddu@gmail.com>
2026-04-10 18:42:31 -0400

    Add files via upload

---
+++ portdetector.py
@@ -0,0 +1,32 @@
+import socket
+from concurrent.futures import ThreadPoolExecutor
+import sys 
+
+if len(sys.argv) != 2:
+    print("Please give a host to scan")
+    exit(1) 
+else:
+    host = sys.argv[1]
+
+def scan(port):
+    print(port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(3)
+
+    result = sock.connect_ex((host, port))
+    sock.close()
+    if result == 0:
+        write_to_file(port)
+    else:
+          return None
+
+def write_to_file(port):
+     with open("openports.txt", "a") as f:
+        f.write(f"{host}:Yea port {port} is listening\n")
+        f.flush()
+
+def scan_all():
+    with ThreadPoolExecutor(max_workers=150) as executor:
+        executor.map(scan, range(1,49152))
+
+scan_all()
